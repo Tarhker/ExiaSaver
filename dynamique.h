5a9d87c3dc22c8b65f04138756c72b6c778da941
zelra <pierre.gilly@viacesi.fr>
2016-12-14 09:57:31 +0100

    Delete architecture

--- dynamique.h
+++
@@ -1,9 +0,0 @@
-#ifndef DYNAMIQUE_H_INCLUDED
-#define DYNAMIQUE_H_INCLUDED
-
-void choixFormatAleatoire();
-void afficherHeure(int heures, int minutes, int secondes);
-void actualisationHeure(int n);
-void finProcessus();
-
-#endif
