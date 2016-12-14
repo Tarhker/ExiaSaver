cbc48cece03defa1b952d98e91280b313877212b
zelra <pierre.gilly@viacesi.fr>
2016-12-14 09:56:53 +0100

    Delete architecture

--- menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_H_INCLUDED
-#define MENU_H_INCLUDED
-
-void lancerEcranDeVeille();
-void afficherHistorique();
-void triChronologique(int jour, int mois, int annee, int heures, int minutes, int secondes);
-
-#endif
